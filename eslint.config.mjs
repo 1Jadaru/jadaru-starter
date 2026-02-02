fe0ece4e676469888fef3fdba4d0f180b240b3f5
James Rutter <rutterj@hotmail.com>
2026-02-02 09:35:41 -0800

    chore: upgrade Next.js to 16.1.6, migrate ESLint to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import nextConfig from "eslint-config-next";
+import prettierConfig from "eslint-config-prettier";
+
+const eslintConfig = [
+  ...nextConfig,
+  prettierConfig,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "prefer-const": "error",
+      "no-console": "warn",
+    },
+  },
+];
+
+export default eslintConfig;
